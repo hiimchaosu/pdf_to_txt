a8335a9153ecaccbd3e8b69f4b3b418c80be1cc3
hiimChaosu <patryksob222@gmail.com>
2024-06-19 20:00:24 +0200

    Added merge button, no function done yet, global root... FOR NOW

--- gui.py
+++ gui.py
@@ -8,29 +8,48 @@ from data_extract import extract_data
 
 def on_drop(event):
     file_path = event.data
+    # One machine had curly braces in the path, so I had to cut them out
+    if file_path.startswith('{') and file_path.endswith('}'):
+        file_path = file_path[1:-1]
+
     file_name = os.path.basename(file_path)
+
     if file_path.endswith(".pdf"):
         exists = check_pdf_existence(file_path)
         if exists:
-            messagebox.showinfo("Result:", "ACCESSIBLE")
+            messagebox.showinfo("Result:", "Done.")
             pdf_to_txt(f"{file_name[:-4]}.pdf", f"{file_name[:-4]}.txt")
             extract_data(f"{file_name[:-4]}.txt", f"{file_name[:-4]}_edited.txt", keyword="Licznik:")
         else:
+            print("The file dropped is non existent.")
             messagebox.showwarning("Warning:", "NONACCESSIBLE / NONEXISTENT")
     else:
+        print("An unknown error occurred in GUI section of the code.")
+        print(file_path)
         messagebox.showerror("Error:", "SOMETHING WENT WRONG. DROP PROPER .PDF FILE!")
 
 def create_gui():
     # Creating window to drag and drop the files into
+    global root
     root = TkinterDnD.Tk()
     root.title("PDF to TXT Converter - Simple Edition")
-    root.geometry("400x200")
+    root.geometry("400x300")
 
-    label = tk.Label(root, text="Drag and drop a PDF file here", width=40, height=10, bg="lightgray")
-    label.pack(pady=20)
+    frame = tk.Frame(root)
+    frame.pack(pady=20)
 
-    # Bind the drop event to the on_drop function
-    label.drop_target_register(DND_FILES)
-    label.dnd_bind('<<Drop>>', on_drop)
+    label = tk.Label(frame, text="Choose an action:")
+    label.pack(pady=5)
+
+    merge_button = tk.Button(frame, text="Merge PDF Files", command=None)
+    merge_button.pack(pady=5)
+
+    drag_label = tk.Label(frame, text="Or drag and drop two PDF files:")
+    drag_label.pack(pady=5)
+
+    drag_button = tk.Label(frame, text="Drag-and-Drop files here", width=30, height=20, bg="lightgray")
+    drag_button.pack(pady=5)
+    drag_button.drop_target_register(DND_FILES)
+    drag_button.dnd_bind('<<Drop>>', on_drop)
 
     root.mainloop()
